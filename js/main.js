5ad11c43c88e98d05aea4c5c03506000fa9a7290
mo3az <mo3az.abdellatif@gmail.com>
2022-02-18 15:29:18 +0200

    page

--- js/main.js
+++ js/main.js
@@ -17,4 +17,42 @@ $(document).ready(function () {
     $("nav").removeClass("active");
     $(".overlay").fadeOut(300);
   });
+  //////////** main slider **//////////
+  var mainswiper = new Swiper(".first-slider .swiper-container", {
+    spaceBetween: 15,
+    loop: true,
+    // autoplay: {
+    //   delay: 5000,
+    // },
+    navigation: {
+      nextEl: ".first-slider .swiper-btn-next",
+      prevEl: ".first-slider .swiper-btn-prev",
+    },
+  });
+  var mainswiper = new Swiper(".second-slider .swiper-container", {
+    slidesPerView:2,
+    spaceBetween: 15,
+    centeredSlides:true,
+    loop: true,
+    // autoplay: {
+    //   delay: 5000,
+    // },
+    navigation: {
+      nextEl: ".second-slider .swiper-btn-next",
+      prevEl: ".second-slider .swiper-btn-prev",
+    },
+  });
+  var mainswiper = new Swiper(".third-slider .swiper-container", {
+    slidesPerView:2,
+    spaceBetween: 15,
+    centeredSlides:true,
+    loop: true,
+    // autoplay: {
+    //   delay: 5000,
+    // },
+    navigation: {
+      nextEl: ".third-slider .swiper-btn-next",
+      prevEl: ".third-slider .swiper-btn-prev",
+    },
+  });
 });
